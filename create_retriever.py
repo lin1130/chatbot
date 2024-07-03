776610bd659ca5fa35b3b581be2954022f2094c8
lin1130 <lf011130@gmail.com>
2024-07-03 16:51:20 -0700

    Day 1

--- create_retriever.py
+++ create_retriever.py
@@ -1,2 +1,19 @@
 # Retrieval Objects: retrival-augmented generation (RAG)
+import dotenv
 from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain.document_loaders.csv_loader import CSVLoader
+
+review_csv_path = "data/reviews.csv"
+review_chroma_path = "chroma_data/"
+
+dotenv.load_dotenv()
+reviews_vector_db = Chroma(
+    persist_directory=review_chroma_path,
+    embedding_function=OpenAIEmbeddings()
+)
+question = """
+Has anyone complained about communication with the hospital staff?
+"""
+relevant_docs = reviews_vector_db.similarity_search(question, k=3)
+output1 = relevant_docs[1].page_content
